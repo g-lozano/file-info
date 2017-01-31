d17ec231d6f7a501fdbc31c4250aa3d7fd0b7432
G Lozano <gfourth1005@gmail.com>
2017-01-31 21:58:19 +0000

    update

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-// to do: 
-// define /readfile router
-// css for button
-
-// option: async upload
-
 var express = require('express')
 var bodyParser = require('body-parser')
-var multer  = require('multer')
+var multer = require('multer')
+var fs = require('fs')
+var upload = multer({ dest: 'uploads/' })
 var app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/readfile', function(req, res) {
-    // return file size, file type?, etc.
-    // handle no file sent
-    res.send('analyzing file')
+app.post('/readfile', upload.single('file'), function(req, res) {
+
+    var type = req.file.mimetype.split('/')[0]
+    var ext = req.file.mimetype.split('/')[1]
+
+    if (req.file) {
+        fs.unlinkSync(req.file.path) //delete file
+        res.json({
+            size: req.file.size,
+            type: type,
+            ext: ext
+        })
+    }
+    else
+        res.json({ error: "No file was uploaded." })
+})
+
+app.get('*', function(req, res) {
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT)
